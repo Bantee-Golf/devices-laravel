044e01f160e1efa417cb39a31075cbd36f3c0352
Shane Perera <shane@shaneperera.com>
2018-10-28 03:46:31 +1100

    Update seeder

--- database/seeds/DevicesTableSeeder.php
+++ database/seeds/DevicesTableSeeder.php
@@ -17,13 +17,21 @@ class DevicesTableSeeder extends Seeder
 				'device_type' => 'apple',
 				'device_push_token' => '23232323232323',
 				'user_id' => 1,
+				'access_token' => Device::newUniqueToken('access_token'),
 			],
 			[
 				'device_id' => 332211,
 				'device_type' => 'android',
 				'device_push_token' => '500500500500500',
-				'user_id' => 2,
-			]
+				'access_token' => Device::newUniqueToken('access_token'),
+			],
+			[
+				'device_id' => 33221155,
+				'device_type' => 'android',
+				'device_push_token' => '500500500500600',
+				'user_id' => 3,
+				'access_token' => Device::newUniqueToken('access_token'),
+			],
 		];
 
 		$this->seedButDontCreateDuplicates($data, Device::class, 'device_id', 'device_id');
